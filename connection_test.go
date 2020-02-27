e69a87e6add6d67a66905e9b1a510984a2b3b808
EnzoBtv <enzo_batrov@hotmail.com>
2020-02-27 17:34:28 -0300

    Changing the test to be more assertive

--- connection_test.go
+++ connection_test.go
@@ -3,12 +3,15 @@ package sequego
 import (
 	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 type databaseConnectionTest struct {
 	user       string
 	password   string
 	dataSource string
+	err        error
 }
 
 func TestConnect(t *testing.T) {
@@ -16,17 +19,21 @@ func TestConnect(t *testing.T) {
 		{user: "root", password: "root", dataSource: "sequego"},
 		{user: "root", password: "rot", dataSource: "sequego"},
 		{user: "root", password: "root", dataSource: "seque"},
-		{user: "", password: "root", dataSource: "seque"},
+		{user: "", password: "root", dataSource: "seque", err: fmt.Errorf("The username (first parameter) is required")},
 		{user: "root", password: "", dataSource: "seque"},
-		{user: "root", password: "root", dataSource: ""},
+		{user: "root", password: "root", dataSource: "", err: fmt.Errorf("The datasource (third parameter) is required")},
 	}
 	for _, test := range tests {
 		testName := fmt.Sprintf("%s %s %s", test.user, test.password, test.dataSource)
 		t.Run(testName, func(t *testing.T) {
 			err := Connect(test.user, test.password, test.dataSource)
 
-			if err != nil {
-				t.Error(err)
+			if test.err != nil {
+				assert.Equal(t, test.err, err)
+			}
+
+			if test.err == nil && err != nil {
+				t.Errorf("Test failed due to %v", err)
 			}
 		})
 	}
--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/EnzoBtv/sequego
 
 go 1.13
 
-require github.com/go-sql-driver/mysql v1.5.0
+require (
+	github.com/go-sql-driver/mysql v1.5.0
+	github.com/stretchr/objx v0.2.0 // indirect
+	github.com/stretchr/testify v1.5.1
+	gopkg.in/yaml.v2 v2.2.8 // indirect
+)
